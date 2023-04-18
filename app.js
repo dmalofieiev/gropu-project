0486285f40d81e53f16fad408b0107e9987df943
Dmytro Malofieiev <maloy34522@gmail.com>
2023-04-18 14:28:36 +0200

    project init

---
+++ app.js
@@ -0,0 +1,49 @@
+require('dotenv').config();
+require('@babel/register');
+
+const ReactDOMServer = require('react-dom/server');
+const React = require('react');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+const express = require('express');
+
+
+const dbCheck = require('./src/middlewares/dbCheck');
+const isAuth = require('./src/middlewares/isAuth');
+
+// Require routes
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Cookie
+const sessionConfig = {
+  name: 'TwitterCookie',
+  store: new FileStore(),
+  secret: process.env.SESSION_SECRET ?? 'Секретное слово',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 9999999,
+    httpOnly: true,
+  },
+};
+app.use(session(sessionConfig)); // Подключаем сессии как middleware.
+app.use('/login', (req, res, next) => {
+  console.log('session=>', req.session);
+  next();
+});
+
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(dbCheck);
+
+// Routes
+
+app.listen(PORT, () => {
+  console.log('Server started');
+});
